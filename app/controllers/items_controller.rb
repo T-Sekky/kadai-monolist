e81745eb8a75dbfbb3d8fb73a18e4e7a5a3760d4
sekky <pocopocoboobee@gmail.com>
2018-11-29 02:45:58 +0000

    want & have

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,17 @@
 class ItemsController < ApplicationController
   before_action :require_user_logged_in
 
+  def show
+    @item = Item.find(params[:id])
+    @want_users = @item.want_users
+    @have_users = @item.have_users
+  end
+
   def new
     @items = []
 
     @keyword = params[:keyword]
-    if @keyword.present? # 
+    if @keyword.present?
       results = RakutenWebService::Ichiba::Item.search({
         keyword: @keyword,
         imageFlag: 1,
@@ -13,16 +19,9 @@ class ItemsController < ApplicationController
       })
 
       results.each do |result|
-        # 扱い易いように Item としてインスタンスを作成する（保存はしない）
         item = Item.find_or_initialize_by(read(result))
         @items << item
       end
     end
-  end
-  
-  def show
-    @item = Item.find(params[:id])
-    @want_users = @item.want_users
-    @have_users = @item.have_users
   end
 end
